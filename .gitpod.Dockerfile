a44878103d08d4906bb16e08f8553a3f282bf7bc
hiromichi_hayashi <6phyphy6@gmail.com>
2022-12-01 06:11:08 +0000

    comment dockerfile

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -5,8 +5,8 @@ USER gitpod
 # Install custom tools, runtime, etc. using apt-get
 # For example, the command below would install "bastet" - a command line tetris clone:
 #
-RUN sudo apt-get -q update && \
-    sudo apt install -yq libopencv-dev python3-opencv && \
-    sudo pip install opencv-python
+# RUN sudo apt-get -q update && \
+#     sudo apt install -yq libopencv-dev python3-opencv && \
+#     sudo pip install opencv-python-headless
 #
 # More information: https://www.gitpod.io/docs/42_config_docker/
--- Day1/get_started_with_images.py
+++ Day1/get_started_with_images.py
@@ -1,20 +1,24 @@
+import os
 import numpy as np
 import cv2
 
-## TODO
+img_path = os.path.join('/', 'workspace', 'opencv-project', 'Day1', 'si.jpg')
 # 画像を読み込む
-img = 
+img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
 # 画像を表示する
+cv2.imshow('sample', img)
 
 # キーボード入力を処理する
-k = 
+k = input()
 if k == 27: # wait for ESC key to exit
     # 現在までに作られた全てのウィンドウを閉じる
+    cv2.destroyAllWindows()
     
 elif k == ord('s'): # wait for 's' key to save and exit
     # 画像を保存する
+    cv2.imwrite('sample.jpg', img)
     
     # 現在までに作られた全てのウィンドウを閉じる
+    cv2.destroyAllWindows()
     
-## TODO
